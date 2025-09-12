152c68de437e37d982730b8a2e29e46dafb52964
إلركس <aliahejje209@gmail.com>
2025-09-12 17:48:52 +0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -202,7 +202,8 @@ local HalfHeight = (SectionChildHeight - (SectionsPadding * 3)) / 2
 
 local MenuWindow = MachoMenuTabbedWindow("VIP", MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y, TabsBarWidth)
 MachoMenuSetKeybind(MenuWindow, 0x14)
-MachoMenuSetAccent(MenuWindow, 120, 140, 160)
+MachoMenuSetAccent(MenuWindow, 255, 0, 0)
+
 
 
 
@@ -5907,6 +5908,7 @@ MachoMenuButton(SettingTabSections[3], "Framework Checker", function()
     local frameworkName = DetectFramework()
     notify("Framework: %s", frameworkName)
 end)
+
 
 
 
